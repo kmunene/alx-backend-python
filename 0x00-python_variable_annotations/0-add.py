4726b7a156dcd392463aa31ab915a63dde53e240
kmunene <kmunene529@gmail.com>
2024-06-06 15:02:59 +0300

    tasks

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    '''returns the sum'''
+    
+    return (a * b)
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def concat(str1: str, str2: str) -> str:
+    '''retruns a concanated string'''
+
+    return f'{str1}{str2}'
