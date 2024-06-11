54e44ecb28d6fc20c655026867571e086ab9fd90
kelvin munene <kmunene529@gmail.com>
2024-06-12 00:48:31 +0300

    Create 2-measure_runtime.py

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""parallel comprehensions"""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    measure rutime
+    """
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.time()
+
+    return end_time - start_time
